302e289b928d60c66e0a2439b0392385f6c87757
Hariprasad <sandy8019331@gmail.com>
2025-05-24 13:01:15 +0530

    Added Test01, 02, 03

---
+++ src/main/java/mainPackage/Test03.java
@@ -0,0 +1,12 @@
+package mainPackage;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ src/test/java/TestPackage1/Test02.java
@@ -0,0 +1,11 @@
+package TestPackage1;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+System.out.println(i);
+	}
+
+}
